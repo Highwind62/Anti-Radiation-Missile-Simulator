9fac786cfa7cf02f8837adf1372f2a3ee84556f2
Highwind62 <zhang927@miamioh.edu>
2024-09-19 18:39:09 -0400

    Xuan 2024/9/19
    Added missile targeting radar

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Missile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public float speed = 0;
+  // Moving direction of missile, set to (0, 0, -1) as default
+  private Vector3 moveDir = new Vector3(0, 0, -1);
+  private Transform targetPos;
 
-    // Update is called once per frame
-    void Update()
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    transform.position += moveDir * speed;  
+    HitCheck();
+  }
+
+  void OnCollisionEnter(Collision collision) 
+  {
+    Debug.Log("Radar in range");
+    string radar = "1S12 Long Track Acquisition Radar";
+    if (collision.gameObject.tag == "Radar") 
     {
-      transform.position += new Vector3 (0, 0, -0.3f);  
+      Debug.Log("Radar in range");
+      targetPos = collision.transform.Find(radar);
+      moveDir = targetPos.position - transform.position;
+      moveDir.Normalize();
     }
+  }
+
+  void HitCheck() 
+  {
+    if (targetPos != null) 
+    {
+      // If missile hit radar, exit editor game mode.
+      if (Vector3.Distance(targetPos.position, transform.position) < 10)
+      {
+        Debug.Log("Missile hit radar.");
+        EditorApplication.isPlaying = false;
+      }
+    }
+  }
 }
