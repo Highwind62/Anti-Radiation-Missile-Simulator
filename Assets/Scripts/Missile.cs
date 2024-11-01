44c7ebe3aac0c8245199dcb087931c3a9d0939a7
Aaron Benjamin <157989230+AaronDBenjamin@users.noreply.github.com>
2024-11-01 13:01:37 -0400

    Dynamic radius with fixed angle side points

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -23,32 +23,59 @@ public class Missile : MonoBehaviour
 
   public float distance3;
 
+ 
+
+
+
   // Start is called before the first frame update
   void Start()
   {
+
+
     distance1 = Vector3.Distance(cube1.transform.position, target.transform.position);
     distance2 = Vector3.Distance(cube2.transform.position, target.transform.position);
     distance3 = Vector3.Distance(cube3.transform.position, target.transform.position);
+
   }
 
   // Update is called once per frame
   void Update()
   {
+    //cube1.transform.position = new Vector3(cube1.transform.position.x, cube1.transform.position.y, target.transform.position.z);
+    //cube2.transform.position = new Vector3(cube2.transform.position.x, cube2.transform.position.y, target.transform.position.z);
+    //cube3.transform.position = new Vector3(cube3.transform.position.x, cube3.transform.position.y, target.transform.position.z);
+
+    cube1.transform.position = new Vector3(cube1.transform.position.x, cube1.transform.position.y, target.transform.position.z);
+    cube2.transform.position = new Vector3( (target.transform.position.z - transform.position.z) * Mathf.Sin((45.0F/180.0F) * Mathf.PI), cube2.transform.position.y, target.transform.position.z);
+    cube3.transform.position = new Vector3( (target.transform.position.z - transform.position.z) * Mathf.Sin((-45.0F/180.0F) * Mathf.PI), cube3.transform.position.y, target.transform.position.z);
+
     distance1 = Vector3.Distance(cube1.transform.position, target.transform.position);
     distance2 = Vector3.Distance(cube2.transform.position, target.transform.position);
     distance3 = Vector3.Distance(cube3.transform.position, target.transform.position);
 
+
+
     if (distance1 < distance2 && distance1 < distance3) {
       transform.position = Vector3.MoveTowards(transform.position, cube1.transform.position, speed);
+      
+
     } else if(distance2 < distance1 && distance2 < distance3){
       transform.position = Vector3.MoveTowards(transform.position, cube2.transform.position, speed);
+
 
     } else if(distance3 < distance1 && distance3 < distance2){
       transform.position = Vector3.MoveTowards(transform.position, cube3.transform.position, speed);
 
+
     } else {
       transform.position = Vector3.MoveTowards(transform.position, cube1.transform.position, speed);
+      
+
     }
+
+
+
+
 
     /*
     transform.position += moveDir * speed;  
