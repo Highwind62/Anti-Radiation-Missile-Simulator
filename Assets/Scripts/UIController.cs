5030472c868a2b2b57ca8356f6af91c9093b5c87
Suri-Zhang <zhang762@miamioh.edu>
2025-02-18 18:22:22 -0500

    fix rest and launch button conflict

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
     //  UI component references
     public Slider speedSlider;       
-    public Text speedText;           
+    public TextMeshProUGUI speedText;           
     public Slider accelerationSlider; 
-    public Text accelerationText;     
+    public TextMeshProUGUI accelerationText;     
     public Button launchButton;       
-    public Button resetButton;        
+    public Button resetButton;
+    public GameObject startPanel;
 
     //  Reference to `Missile` script, so UI can control the missile
     public Missile missileScript;
@@ -22,6 +24,8 @@ public class UIController : MonoBehaviour
         launchButton.onClick.AddListener(LaunchMissile);
         resetButton.onClick.AddListener(ResetSimulation);
 
+        startPanel.SetActive(true);
+
         // Initialize UI display values
         speedText.text = "Speed: " + speedSlider.value.ToString("F1");
         accelerationText.text = "Acceleration: " + accelerationSlider.value.ToString("F1");
@@ -32,6 +36,7 @@ public class UIController : MonoBehaviour
         //  Update speed value in UI and modify `speed` in `Missile.cs`
         speedText.text = "Speed: " + value.ToString("F1");
         missileScript.speed = value;
+        CheckLaunchReady();
     }
 
     void UpdateAcceleration(float value)
@@ -39,10 +44,18 @@ public class UIController : MonoBehaviour
         //  Update acceleration value in UI and modify `acceleration` in `Missile.cs`
         accelerationText.text = "Acceleration: " + value.ToString("F1");
         missileScript.acceleration = value;
+        CheckLaunchReady();
     }
 
+
+    void CheckLaunchReady()
+    {
+        // When player adjust speed or acceleration, `Launch` button starts work
+        launchButton.interactable = (missileScript.speed > 0 || missileScript.acceleration > 0);
+    }
     void LaunchMissile()
     {
+        startPanel.SetActive(false);
         //  Call `Launch()` in `Missile.cs` to launch the missile
         missileScript.Launch();
     }
@@ -51,5 +64,8 @@ public class UIController : MonoBehaviour
     {
         //  Call `ResetPosition()` in `Missile.cs` to reset the missile
         missileScript.ResetPosition();
+
+        startPanel.SetActive(true);
+
     }
 }
